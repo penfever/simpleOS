2603420c61db85ef2463cff35eed453a6435ecbc
Ben Feuer <penfever@hotmail.com>
2021-01-27 09:59:38 -0500

    makefile, directories

---
+++ src/word-count/MAKEFILE.mk
@@ -0,0 +1,4 @@
+CC = gcc
+CFLAGS += -g -Wall -Wextra -pedantic
+
+all: word-count
---
+++ src/word-count/word-count.c
@@ -0,0 +1,38 @@
+/*   
+Write a program named word-count.c in C, that uses file
+  I/O to read a text file whose name is specified on the command line
+  to the program.  
+  
+  All code for word-count should be placed in a
+  src/word-count directory.
+
+Lines are delimited by a newline character
+ (or additionally and optionally for development on Windows
+   computers, by a carriage-return immediately followed by a linefeed). 
+
+   No other characters should be treated specially (i.e., punctuation,
+  hyphens, etc. should just be considered as non-white-space
+  characters that should be treated as part of words).
+
+  case sensitive
+
+  Upon reaching
+  end-of-file, the program will output to stdout: (1) the number of
+  lines in the input file (include blank lines in the count of the
+  number of lines), 
+
+  (2) the total number of words in the input file
+  (*not* the number of unique words), 
+
+  (3) a list of each unique word
+  in the input file along with the number of times that word appears
+  in the file.
+  
+   */
+
+#include <stdio.h>
+
+int main(void){
+    printf("Hello, world.\n");
+    return 0;
+}
